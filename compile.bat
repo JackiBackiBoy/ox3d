44a57d5ec4f0b69283c4cfe2727c24e1f3b6d16e
JackiBackiBoy <TheGameTail@gmail.com>
2022-02-27 18:56:26 +0100

    Removed compile.bat

--- compile.bat
+++
@@ -1,3 +0,0 @@
-C:/VulkanSDK/1.3.204.0/Bin/glslc.exe assets/shaders/lightingShader.vert -o assets/shaders/lightingShaderVert.spv
-C:/VulkanSDK/1.3.204.0/Bin/glslc.exe assets/shaders/lightingShader.frag -o assets/shaders/lightingShaderFrag.spv
-pause
