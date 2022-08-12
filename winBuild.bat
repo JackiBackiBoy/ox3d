22160cc6c2bd55c398e5f4504a90bbbcafebd0f8
Jack Henrikson <TheGameTail@gmail.com>
2022-08-12 14:11:37 +0200

    Fix incoming

--- winBuild.bat
+++ winBuild.bat
@@ -1,9 +1,4 @@
 if not exist build mkdir build
 cd build
-cmake -S ../ -B . -G "MinGW Makefiles"
+cmake -S ../ -B . -G "MinGW Makefiles" -D GLFW_BUILD_DOCS=OFF
 mingw32-make
-mingw32-make Shaders
-mingw32-make install
-cd bin
-sandbox.exe
-cd ../..
